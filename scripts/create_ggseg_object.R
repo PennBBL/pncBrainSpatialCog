87a2996d8a7f3a95f72fe30f69c2f452b5a66e48
Ellyn Butler <ellynrbutler@gmail.com>
2021-01-29 14:26:29 -0500

    all modalities

--- scripts/create_ggseg_object.R
+++ scripts/create_ggseg_object.R
@@ -1,7 +1,7 @@
 ### This script attempts to recreate the ggseg object without holes
 ###
 ### Ellyn Butler
-### December 7, 2020
+### December 7, 2020 - December 14, 2020
 
 library(ggseg) # v 1.6.00
 library(ggsegExtra) # v 1.5.31
@@ -9,9 +9,46 @@ library(ggseg3d) # 1.6.02
 library(tidyverse) # v 1.3.0
 
 # convert miccai to fsaverage5
-mri_surf2surf_rereg(subject = "fsaverage", annot = "mic", hemi = "lh")
-mri_surf2surf_rereg(subject = "fsaverage", annot = "mic", hemi = "rh")
+mri_surf2surf_rereg(subject = 'fsaverage', annot = 'mic', hemi = 'lh',
+  target_subject='fsaverage5', output_dir='/Applications/freesurfer/7.1.1/subjects/fsaverage5/label')
+mri_surf2surf_rereg(subject = 'fsaverage', annot = 'mic', hemi = 'rh',
+  target_subject='fsaverage5', output_dir='/Applications/freesurfer/7.1.1/subjects/fsaverage5/label')
 
 # Make 3d miccai atlas
-mic_3d <- make_aparc_2_3datlas(annot = "mic",
-  output_dir = "~/Documents/pncBrainSpatialCog/data/")
+mic_3d <- make_aparc_2_3datlas(annot = 'mic',
+  output_dir = '~/Documents/pncBrainSpatialCog/ggseg_atlas')
+
+# View atlas
+#ggseg3d(atlas = mic_3d)
+
+# Look at atlas data contents
+#unnest(mic_3d, ggseg_3d)
+
+# Inspect unique values of "region"
+#unnest(mic_3d, ggseg_3d) %>%
+#  select(region) %>%
+#  unique() %>%
+#  unlist()
+
+# Edit out "
+mic_3d <- unnest(mic_3d, ggseg_3d) %>%
+    mutate(region = gsub("\"", "", region), label = gsub("\"", "", label),
+      annot = gsub("\"", "", annot), atlas = "mic_3d") %>%
+  nest_by(atlas, surf, hemi, .key = "ggseg_3d") %>%
+  as_ggseg3d_atlas()
+
+# View atlas (still has some holes, and some interweaving)
+#ggseg3d(atlas = mic_3d, hemisphere = "left", surface = "inflated")
+#ggseg3d(atlas = mic_3d, hemisphere = "right", surface = "inflated")
+
+#ggseg3d(atlas = mic_3d, hemisphere = "left", surface = "LCBC")
+#ggseg3d(atlas = mic_3d, hemisphere = "right", surface = "LCBC")
+
+#ggseg3d(atlas = mic_3d, hemisphere = "left", surface = "white")
+#ggseg3d(atlas = mic_3d, hemisphere = "right", surface = "white")
+
+
+################################### 2D Atlas ###################################
+
+mic <- make_ggseg3d_2_ggseg(mic_3d, steps = 1:7, smoothness = 2, tolerance = .5,
+  output_dir = '~/Documents/pncBrainSpatialCog/ggseg_atlas')
--- scripts/ggseg_effsize_by_cogfactor_rank.R
+++ scripts/ggseg_effsize_by_cogfactor_rank.R
@@ -2,7 +2,7 @@
 ### factor score on the brain
 ###
 ### Ellyn Butler
-### November 3, 2020 - December 14, 2020
+### November 3, 2020 - January 29, 2021
 
 library('ggseg') # v 1.1.5 >>> 1.6.00
 library('ggplot2') # v 3.3.2 >>> 3.3.2
@@ -12,173 +12,184 @@ library('tidyverse') # v 1.3.0 >>> 1.3.0
 library('R.utils') # v 2.10.1 >>> 2.10.1
 library('scales') # v 1.1.1 >>> 1.1.1
 
-load('~/Documents/ggsegExtra/data/micCort.rda')
-eff_df <- read.csv('~/Documents/pncBrainSpatialCog/results/loadings/volume_model_output_HiLo2.csv')
-eff_df <- eff_df[,c('Region', 'StdEffectRescale', 'Score')]
-names(eff_df)[names(eff_df) == 'Region'] <- 'region'
-
-eff_df$region <- gsub('vol_', '', eff_df$region)
-eff_df$region <- recode(eff_df$region, 'Hippocampus'='Hipp', 'Amygdala'='Amy',
-  'Pallidum'='Pall', 'Putamen'='Put', 'Accumbens_Area'='NA', 'Caudate'='Cau',
-  'Thalamus_Proper'='Thal')
-
-eff_df$region <- as.character(eff_df$region)
-eff_df$region <- recode(eff_df$region, 'PIns'='Pins')
-
-micCort <- micCort %>%
-   rename(region = area) %>%
-   mutate(ggseg = map(ggseg, ~ mutate(.x, .subid = 1)))
-
-base_atlas = micCort %>%
-  select(region, hemi) %>%
-  distinct() %>%
-  na.omit()
-
-micCort2 <- as_ggseg_atlas(micCort)
-
-eff_df$region <- recode(eff_df$region, 'Hipp'='hippocampus',
-  'Thalamus'='thalamus proper', 'Put'='putamen', 'Amy'='amygdala',
-  'Pall'='pallidum', 'Cau'='caudate')
-
-subcortical <- c('hippocampus', 'thalamus proper', 'putamen', 'amygdala',
-  'pallidum', 'caudate')
-
-eff_df <- eff_df[eff_df$region %in% c(micCort2$region, subcortical), ]
-
-compcog_df <- eff_df[eff_df$Score == 'F1_Exec_Comp_Cog_Accuracy', ]
-row.names(compcog_df) <- 1:nrow(compcog_df)
-soccog_df <- eff_df[eff_df$Score == 'F2_Social_Cog_Accuracy', ]
-row.names(soccog_df) <- 1:nrow(soccog_df)
-mem_df <- eff_df[eff_df$Score == 'F3_Memory_Accuracy', ]
-row.names(mem_df) <- 1:nrow(mem_df)
-
-compcog_df$StdEffectRescale <- rescale(compcog_df$StdEffectRescale)
-soccog_df$StdEffectRescale <- rescale(soccog_df$StdEffectRescale)
-mem_df$StdEffectRescale <- rescale(mem_df$StdEffectRescale)
-
-compcog_df <- compcog_df %>% mutate(quantile = ntile(StdEffectRescale, 5))
-soccog_df <- soccog_df %>% mutate(quantile = ntile(StdEffectRescale, 5))
-mem_df <- mem_df %>% mutate(quantile = ntile(StdEffectRescale, 5))
-
-compcog_df$quantile <- ordered(compcog_df$quantile, 1:5)
-soccog_df$quantile <- ordered(soccog_df$quantile, 1:5)
-mem_df$quantile <- ordered(mem_df$quantile, 1:5)
 
 
-labels <- micCort2[micCort2$hemi == "left" & micCort2$region %in% eff_df$region, ] %>%
-  unnest(cols = ggseg) %>%
-  group_by(region) %>%
-  summarise(.lat =  mean(.lat), .long = mean(.long))
+modalities <- c('CBF', 'GMD', 'NBack', 'ReHo', 'Volume')
+substrings <- c('vol_', 'as\\.matrix\\(srcbf\\)pcasl_jlf_cbf_', '_assr_SelfReg',
+  'as\\.matrix\\(srgmd\\)mprage_jlf_gmd_', 'as\\.matrix\\(srreho\\)rest_jlf_reho_',
+  'as\\.matrix\\(srNBACK\\)sigchange_contrast4_2back0back_mean_miccai_ave_', '1', '2')
 
-bigstdeffectrescale_compcog <- compcog_df[compcog_df$StdEffectRescale > .5, 'region']
-bigstdeffectrescale_soccog <- soccog_df[soccog_df$StdEffectRescale > .5, 'region']
-bigstdeffectrescale_mem <- mem_df[mem_df$StdEffectRescale > .5, 'region']
+for (modal in modalities) {
+  load('~/Documents/ggsegExtra/data/micCort.rda')
+  eff_df <- read.csv(paste0('~/Documents/pncBrainSpatialCog/results/loadings/', modal, '_model_output_HiLo2.csv'))
 
-labels_compcog <- labels[labels$region %in% bigstdeffectrescale_compcog, ]
-labels_soccog <- labels[labels$region %in% bigstdeffectrescale_soccog, ]
-labels_mem <- labels[labels$region %in% bigstdeffectrescale_mem, ]
+  if ('X' %in% names(eff_df)) { names(eff_df)[names(eff_df) == 'X'] <- 'Region' }
+  if ('score' %in% names(eff_df)) { names(eff_df)[names(eff_df) == 'score'] <- 'Score' }
+  eff_df <- eff_df[,c('Region', 'StdEffectRescale', 'Score')]
+  names(eff_df)[names(eff_df) == 'Region'] <- 'region'
+
+  for (substr in substrings) {
+    eff_df$region <- gsub(substr, '', eff_df$region)
+  }
+  eff_df$region <- recode(eff_df$region, 'Hippocampus'='Hipp', 'Amygdala'='Amy',
+    'Pallidum'='Pall', 'Putamen'='Put', 'Accumbens_Area'='NA', 'Caudate'='Cau',
+    'Thalamus_Proper'='Thal')
+
+  eff_df$region <- as.character(eff_df$region)
+  eff_df$region <- recode(eff_df$region, 'PIns'='Pins')
+
+  micCort <- micCort %>%
+     rename(region = area) %>%
+     mutate(ggseg = map(ggseg, ~ mutate(.x, .subid = 1)))
+
+  base_atlas = micCort %>%
+    select(region, hemi) %>%
+    distinct() %>%
+    na.omit()
+
+  micCort2 <- as_ggseg_atlas(micCort)
+
+  eff_df$region <- recode(eff_df$region, 'Hipp'='hippocampus',
+    'Thalamus'='thalamus proper', 'Put'='putamen', 'Amy'='amygdala',
+    'Pall'='pallidum', 'Cau'='caudate')
+
+  subcortical <- c('hippocampus', 'thalamus proper', 'putamen', 'amygdala',
+    'pallidum', 'caudate')
+
+  eff_df <- eff_df[eff_df$region %in% c(micCort2$region, subcortical), ]
+
+  compcog_df <- eff_df[eff_df$Score == 'F1_Exec_Comp_Cog_Accuracy', ]
+  row.names(compcog_df) <- 1:nrow(compcog_df)
+  soccog_df <- eff_df[eff_df$Score == 'F2_Social_Cog_Accuracy', ]
+  row.names(soccog_df) <- 1:nrow(soccog_df)
+  mem_df <- eff_df[eff_df$Score == 'F3_Memory_Accuracy', ]
+  row.names(mem_df) <- 1:nrow(mem_df)
+
+  compcog_df$StdEffectRescale <- rescale(compcog_df$StdEffectRescale)
+  soccog_df$StdEffectRescale <- rescale(soccog_df$StdEffectRescale)
+  mem_df$StdEffectRescale <- rescale(mem_df$StdEffectRescale)
+
+  compcog_df <- compcog_df %>% mutate(quantile = ntile(StdEffectRescale, 5))
+  soccog_df <- soccog_df %>% mutate(quantile = ntile(StdEffectRescale, 5))
+  mem_df <- mem_df %>% mutate(quantile = ntile(StdEffectRescale, 5))
+
+  compcog_df$quantile <- ordered(compcog_df$quantile, 1:5)
+  soccog_df$quantile <- ordered(soccog_df$quantile, 1:5)
+  mem_df$quantile <- ordered(mem_df$quantile, 1:5)
 
 
+  labels <- micCort2[micCort2$hemi == "left" & micCort2$region %in% eff_df$region, ] %>%
+    unnest(cols = ggseg) %>%
+    group_by(region) %>%
+    summarise(.lat =  mean(.lat), .long = mean(.long))
 
-p_compcog_cort <- ggseg(compcog_df[compcog_df$region %in% micCort2$region, ], atlas='micCort2',
-    mapping=aes(fill=quantile), hemisphere='left', size=.1, colour='black') +
-  #scale_fill_gradient(low = 'cornsilk', high = 'red4') +
-  scale_fill_brewer(palette=8, na.translate=FALSE) +
-  ggtitle('Complex Cognition') +
-  theme(text=element_text(size=14), axis.title.x=element_blank(),
-    axis.text.x=element_blank(), legend.position='none') +
-  ggrepel::geom_label_repel(data = labels_compcog, inherit.aes = FALSE, size=3,
-    mapping = aes(x = .long, y=.lat, label=region), label.size = NA,
-    label.padding=.1, na.rm=TRUE, fill = alpha(c("white"),0.5))
+  bigstdeffectrescale_compcog <- compcog_df[compcog_df$StdEffectRescale > .5, 'region']
+  bigstdeffectrescale_soccog <- soccog_df[soccog_df$StdEffectRescale > .5, 'region']
+  bigstdeffectrescale_mem <- mem_df[mem_df$StdEffectRescale > .5, 'region']
 
-p_soccog_cort <- ggseg(soccog_df[soccog_df$region %in% micCort2$region, ], atlas='micCort2',
-    mapping=aes(fill=quantile), hemisphere='left', size=.1, colour='black') +
-  #scale_fill_gradient(low = 'cornsilk', high = 'red4') +
-  scale_fill_brewer(palette=8, na.translate=FALSE) +
-  ggtitle('Social Cognition') +
-  theme(text=element_text(size=14), axis.title.x=element_blank(),
-    axis.text.x=element_blank(), legend.position='none') +
-  ggrepel::geom_label_repel(data = labels_soccog, inherit.aes = FALSE, size=3,
-    mapping = aes(x = .long, y=.lat, label=region), label.size = NA,
-    label.padding=.1, na.rm=TRUE, fill = alpha(c("white"),0.5))
-
-p_mem_cort <- ggseg(mem_df[mem_df$region %in% micCort2$region, ], atlas='micCort2',
-    mapping=aes(fill=quantile), hemisphere='left', size=.1, colour='black') +
-  #scale_fill_gradient(low = 'cornsilk', high = 'red4') +
-  scale_fill_brewer(palette=8, na.translate=FALSE) +
-  ggtitle('Memory') +
-  theme(text=element_text(size=14), axis.title.x=element_blank(),
-    axis.text.x=element_blank(), legend.position='none') +
-  ggrepel::geom_label_repel(data = labels_mem, inherit.aes = FALSE, size=3,
-    mapping = aes(x = .long, y=.lat, label=region), label.size = NA,
-    label.padding=.1, na.rm=TRUE, fill = alpha(c("white"),0.5))
-
-
-#############################################################################
-
-##### Subcortical
-aseg <- as_ggseg_atlas(aseg)
-aseg_data <- aseg
-aseg_data <- as_ggseg_atlas(aseg_data[aseg_data$region %in% subcortical,])
-
-labels <- aseg_data[aseg_data$hemi == "right" & aseg_data$region %in% subcortical, ] %>%
-  unnest(cols = ggseg) %>%
-  group_by(region) %>%
-  summarise(.lat =  mean(.lat), .long = mean(.long))
-
-labels_compcog <- labels[labels$region %in% bigstdeffectrescale_compcog, ]
-labels_soccog <- labels[labels$region %in% bigstdeffectrescale_soccog, ]
-labels_mem <- labels[labels$region %in% bigstdeffectrescale_mem, ]
-
-compcog_subcort_df <- compcog_df[compcog_df$region %in% subcortical, ]
-soccog_subcort_df <- soccog_df[soccog_df$region %in% subcortical, ]
-mem_subcort_df <- mem_df[mem_df$region %in% subcortical, ]
-
-compcog_subcort_df$quantile <- ordered(compcog_subcort_df$quantile, 1:5)
-soccog_subcort_df$quantile <- ordered(soccog_subcort_df$quantile, 1:5)
-mem_subcort_df$quantile <- ordered(mem_subcort_df$quantile, 1:5)
-levels(compcog_subcort_df$quantile) <- 1:5 # Doesn't translate to plot
-levels(soccog_subcort_df$quantile) <- 1:5
-levels(mem_subcort_df$quantile) <- 1:5
-
-p_compcog <- ggseg(compcog_subcort_df, atlas='aseg',
-    hemisphere=c('left', 'right'), mapping=aes(fill=quantile), size=.1, colour='black') +
-  scale_fill_brewer(palette=8, na.translate=FALSE, guide=guide_legend(reverse=TRUE), limits=1:5) +
-  theme(text=element_text(size=14), axis.title.x=element_blank(),
-    axis.text.x=element_blank()) +
-  ggrepel::geom_label_repel(data = labels_compcog, inherit.aes = FALSE, size=3,
-    mapping = aes(x = .long, y=.lat, label=region), label.size = NA,
-    label.padding=.1, na.rm=TRUE, fill = alpha(c("white"),0.5)) #+
-  #geom_sf_label(aes(label = subcortical),
-  #  alpha = .8, show.legend = FALSE, fill = alpha(c("white"),0.5))
-
-p_soccog <- ggseg(soccog_subcort_df, atlas='aseg',
-    hemisphere=c('left', 'right'), mapping=aes(fill=quantile), size=.1, colour='black') +
-  scale_fill_brewer(palette=8, na.translate=FALSE, guide=guide_legend(reverse=TRUE), limits=1:5) +
-  theme(text=element_text(size=14), axis.title.x=element_blank(),
-    axis.text.x=element_blank()) +
-  ggrepel::geom_label_repel(data = labels_soccog, inherit.aes = FALSE, size=3,
-    mapping = aes(x = .long, y=.lat, label=region), label.size = NA,
-    label.padding=.1, na.rm=TRUE, fill = alpha(c("white"),0.5))
-
-p_mem <- ggseg(mem_subcort_df, atlas='aseg',
-    hemisphere=c('left', 'right'), mapping=aes(fill=quantile), size=.1, colour='black') +
-  scale_fill_brewer(palette=8, na.translate=FALSE, guide=guide_legend(reverse=TRUE), limits=1:5) +
-  theme(text=element_text(size=14), axis.title.x=element_blank(),
-    axis.text.x=element_blank()) +
-  ggrepel::geom_label_repel(data = labels_mem, inherit.aes = FALSE, size=3,
-    mapping = aes(x = .long, y=.lat, label=region), label.size = NA,
-    label.padding=.1, na.rm=TRUE, fill = alpha(c("white"),0.5))
-
-p <- ggarrange(ggarrange(p_compcog_cort, p_compcog, nrow=2), ggarrange(p_mem_cort,
-  p_mem, nrow=2), ggarrange(p_soccog_cort, p_soccog, nrow=2), ncol=3)
-
-pdf(file='~/Documents/pncBrainSpatialCog/plots/cogFactorsBrainReScale_quantile.pdf', width=17.7, height=5.512)
-p
-dev.off()
+  labels_compcog <- labels[labels$region %in% bigstdeffectrescale_compcog, ]
+  labels_soccog <- labels[labels$region %in% bigstdeffectrescale_soccog, ]
+  labels_mem <- labels[labels$region %in% bigstdeffectrescale_mem, ]
 
 
 
+  p_compcog_cort <- ggseg(compcog_df[compcog_df$region %in% micCort2$region, ], atlas='micCort2',
+      mapping=aes(fill=quantile), hemisphere='left', size=.1, colour='black') +
+    #scale_fill_gradient(low = 'cornsilk', high = 'red4') +
+    scale_fill_brewer(palette=8, na.translate=FALSE) +
+    ggtitle(paste0(modal, ': Complex Cognition')) +
+    theme(text=element_text(size=14), axis.title.x=element_blank(),
+      axis.text.x=element_blank(), legend.position='none') +
+    ggrepel::geom_label_repel(data = labels_compcog, inherit.aes = FALSE, size=3,
+      mapping = aes(x = .long, y=.lat, label=region), label.size = NA,
+      label.padding=.1, na.rm=TRUE, fill = alpha(c("white"),0.5))
+
+  p_soccog_cort <- ggseg(soccog_df[soccog_df$region %in% micCort2$region, ], atlas='micCort2',
+      mapping=aes(fill=quantile), hemisphere='left', size=.1, colour='black') +
+    #scale_fill_gradient(low = 'cornsilk', high = 'red4') +
+    scale_fill_brewer(palette=8, na.translate=FALSE) +
+    ggtitle(paste0(modal, ': Social Cognition')) +
+    theme(text=element_text(size=14), axis.title.x=element_blank(),
+      axis.text.x=element_blank(), legend.position='none') +
+    ggrepel::geom_label_repel(data = labels_soccog, inherit.aes = FALSE, size=3,
+      mapping = aes(x = .long, y=.lat, label=region), label.size = NA,
+      label.padding=.1, na.rm=TRUE, fill = alpha(c("white"),0.5))
+
+  p_mem_cort <- ggseg(mem_df[mem_df$region %in% micCort2$region, ], atlas='micCort2',
+      mapping=aes(fill=quantile), hemisphere='left', size=.1, colour='black') +
+    #scale_fill_gradient(low = 'cornsilk', high = 'red4') +
+    scale_fill_brewer(palette=8, na.translate=FALSE) +
+    ggtitle(paste0(modal, ': Memory')) +
+    theme(text=element_text(size=14), axis.title.x=element_blank(),
+      axis.text.x=element_blank(), legend.position='none') +
+    ggrepel::geom_label_repel(data = labels_mem, inherit.aes = FALSE, size=3,
+      mapping = aes(x = .long, y=.lat, label=region), label.size = NA,
+      label.padding=.1, na.rm=TRUE, fill = alpha(c("white"),0.5))
+
+
+  #############################################################################
+
+  ##### Subcortical
+  aseg <- as_ggseg_atlas(aseg)
+  aseg_data <- aseg
+  aseg_data <- as_ggseg_atlas(aseg_data[aseg_data$region %in% subcortical,])
+
+  labels <- aseg_data[aseg_data$hemi == "right" & aseg_data$region %in% subcortical, ] %>%
+    unnest(cols = ggseg) %>%
+    group_by(region) %>%
+    summarise(.lat =  mean(.lat), .long = mean(.long))
+
+  labels_compcog <- labels[labels$region %in% bigstdeffectrescale_compcog, ]
+  labels_soccog <- labels[labels$region %in% bigstdeffectrescale_soccog, ]
+  labels_mem <- labels[labels$region %in% bigstdeffectrescale_mem, ]
+
+  compcog_subcort_df <- compcog_df[compcog_df$region %in% subcortical, ]
+  soccog_subcort_df <- soccog_df[soccog_df$region %in% subcortical, ]
+  mem_subcort_df <- mem_df[mem_df$region %in% subcortical, ]
+
+  #compcog_subcort_df$quantile <- ordered(compcog_subcort_df$quantile, 1:5)
+  #soccog_subcort_df$quantile <- ordered(soccog_subcort_df$quantile, 1:5)
+  #mem_subcort_df$quantile <- ordered(mem_subcort_df$quantile, 1:5)
+  #levels(compcog_subcort_df$quantile) <- 1:5 # Doesn't translate to plot
+  #levels(soccog_subcort_df$quantile) <- 1:5
+  #levels(mem_subcort_df$quantile) <- 1:5
+
+  p_compcog <- ggseg(compcog_subcort_df, atlas='aseg',
+      hemisphere=c('left', 'right'), mapping=aes(fill=quantile), size=.1, colour='black') +
+    scale_fill_brewer(palette=8, na.translate=FALSE, guide=guide_legend(reverse=TRUE), limits=1:5) +
+    theme(text=element_text(size=14), axis.title.x=element_blank(),
+      axis.text.x=element_blank()) +
+    ggrepel::geom_label_repel(data = labels_compcog, inherit.aes = FALSE, size=3,
+      mapping = aes(x = .long, y=.lat, label=region), label.size = NA,
+      label.padding=.1, na.rm=TRUE, fill = alpha(c("white"),0.5)) #+
+    #geom_sf_label(aes(label = subcortical),
+    #  alpha = .8, show.legend = FALSE, fill = alpha(c("white"),0.5))
+
+  p_soccog <- ggseg(soccog_subcort_df, atlas='aseg',
+      hemisphere=c('left', 'right'), mapping=aes(fill=quantile), size=.1, colour='black') +
+    scale_fill_brewer(palette=8, na.translate=FALSE, guide=guide_legend(reverse=TRUE), limits=1:5) +
+    theme(text=element_text(size=14), axis.title.x=element_blank(),
+      axis.text.x=element_blank()) +
+    ggrepel::geom_label_repel(data = labels_soccog, inherit.aes = FALSE, size=3,
+      mapping = aes(x = .long, y=.lat, label=region), label.size = NA,
+      label.padding=.1, na.rm=TRUE, fill = alpha(c("white"),0.5))
+
+  p_mem <- ggseg(mem_subcort_df, atlas='aseg',
+      hemisphere=c('left', 'right'), mapping=aes(fill=quantile), size=.1, colour='black') +
+    scale_fill_brewer(palette=8, na.translate=FALSE, guide=guide_legend(reverse=TRUE), limits=1:5) +
+    theme(text=element_text(size=14), axis.title.x=element_blank(),
+      axis.text.x=element_blank()) +
+    ggrepel::geom_label_repel(data = labels_mem, inherit.aes = FALSE, size=3,
+      mapping = aes(x = .long, y=.lat, label=region), label.size = NA,
+      label.padding=.1, na.rm=TRUE, fill = alpha(c("white"),0.5))
+
+  p <- ggarrange(ggarrange(p_compcog_cort, p_compcog, nrow=2), ggarrange(p_mem_cort,
+    p_mem, nrow=2), ggarrange(p_soccog_cort, p_soccog, nrow=2), ncol=3)
+
+  pdf(file=paste0('~/Documents/pncBrainSpatialCog/plots/', modal, '_cogFactorsBrainReScale_quantile.pdf'), width=17.7, height=5.512)
+  print(p)
+  dev.off()
+}
 
 
 
